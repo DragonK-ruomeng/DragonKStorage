a079b1d1292d50e703bf7218c1d1b2da674f7dc5
rm_dragonk <1691660095@qq.com>
2020-06-30 14:20:29 +0800

    1

---
+++ src/test/java/com/tedu/zwl/ZwlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tedu.zwl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
